81d3a6901ed457df525f738d258d253432ff6640
kinimini108 <136805120+kinimini108@users.noreply.github.com>
2023-10-16 01:11:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,111 @@
+const readline = require("readline");
+const Todo = require("./todo");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const todo = new Todo();
+
+function displayMenu() {
+  console.log(`Todo a List Application 
+Please Choose One of These Tasks.`);
+  console.log(`Add / Update / Delete / List / Close`);
+}
+
+function promptUser() {
+  rl.question("Enter your choice: ", (choice) => {
+    Choices(choice);
+  });
+}
+function Choices(choice) {
+  switch (choice) {
+    case "Add":
+    case "add":
+      addTask();
+      break;
+    case "Update":
+    case "update":
+      updateTask();
+      break;
+    case "Delete":
+    case "delete":
+      deleteTask();
+      break;
+    case "List":
+    case "Lists":
+    case "list":
+    case "lists":
+      listTasks();
+      break;
+    case "Close":
+    case "cls":
+    case "exit":
+    case "close":
+      rl.close();
+      console.log("See you later✌️");
+      break;
+    default:
+      console.log("Invalid choice. Please try again.");
+      promptUser();
+  }
+}
+function addTask() {
+  rl.question("Enter a Task: ", (task) => {
+    // The user's input is then validated using a regular expression to ensure that it only contains non-digit characters.
+    if (/^\D+$/.test(task)) {
+      try {
+        todo.addTask(task);
+        console.log("Task added successfully.✅");
+      } catch (error) {
+        console.log("Error:", error.message);
+      }
+    } else {
+      console.log("Invalid input. Please enter a task without numbers.");
+    }
+    promptUser();
+  });
+}
+
+function updateTask() {
+  rl.question("Enter ID to update Task: ", (taskId) => {
+    rl.question("Enter the updated task: ", (task) => {
+      try {
+        todo.updateTask(taskId, task);
+        console.log("Task updated successfully.✅");
+      } catch (error) {
+        console.log("Error:", error.message);
+      }
+      promptUser();
+    });
+  });
+}
+
+function deleteTask() {
+  rl.question("Enter ID to delete Task: ", (taskId) => {
+    try {
+      todo.deleteTask(taskId);
+      console.log("Task deleted successfully.✅");
+    } catch (error) {
+      console.log("Error:", error.message);
+    }
+    promptUser();
+  });
+}
+
+function listTasks() {
+  const tasks = todo.listTasks();
+  if (tasks.length === 0) {
+    console.log("No tasks found.");
+  } else {
+    console.log("Here is The List Of Tasks:");
+    tasks.forEach((task) => {
+      console.log(`ID: ${task.id} Name: ${task.task}`);
+    });
+  }
+  promptUser();
+}
+
+displayMenu();
+promptUser();
---
+++ todo.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+
+class Todo {
+  constructor() {
+    this.tasks = this.loadTasks();
+  }
+
+  loadTasks() {
+    try {
+      const tasksData = fs.readFileSync("task.json", "utf8");
+      return JSON.parse(tasksData);
+    } catch (error) {
+        console.error("Error loading tasks:", error);
+      return [];
+    }
+  }
+
+  saveTasks() {
+    const tasksData = JSON.stringify(this.tasks, null, 2);
+    fs.writeFileSync("task.json", tasksData);
+  }
+
+  addTask(task) {
+    if (!task) {
+      throw new Error("Task cannot be empty.");
+    }
+    const randomNumber = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id: randomNumber.toString(), task: task };
+    this.tasks.push(newTask);
+
+    this.saveTasks();
+  }
+
+  updateTask(taskId, updatedTask) {
+    const task = this.findTaskById(taskId);
+    if (!task) {
+      throw new Error("Task not found.");
+    }
+    task.task = updatedTask;
+    this.saveTasks();
+  }
+
+  deleteTask(taskId) {
+    const taskIndex = this.findTaskIndexById(taskId);
+    if (taskIndex === -1) {
+      throw new Error("Task not found.");
+    }
+    this.tasks.splice(taskIndex, 1);
+
+    this.saveTasks();
+  }
+  findTaskById(taskId) {
+    return this.tasks.find((task) => task.id === taskId);
+  }
+
+  findTaskIndexById(taskId) {
+    return this.tasks.findIndex((task) => task.id === taskId);
+  }
+
+  listTasks() {
+    return this.tasks;
+  }
+}
+
+module.exports = Todo;
